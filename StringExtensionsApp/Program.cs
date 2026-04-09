6c7bd273d1d48675cc4afbb72bef7a40eaf4bd73
Toluwalase Fasoyin <tfasoyi2@my.centennialcollege.ca>
2026-04-09 15:21:06 -0400

    Add StringExtensionsApp console app

---
+++ StringExtensionsApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using StringExtensionsLib;
+
+string[] testStrings = { "Hello", "world", "Azure", "devops" };
+
+foreach (string s in testStrings)
+{
+    Console.WriteLine($"\"{s}\" starts with uppercase: {s.StartsWithUpperCase()}");
+}
