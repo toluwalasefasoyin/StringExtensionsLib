0cf84fcc56832f89a5e5515747d13ae8b924ba63
Toluwalase Fasoyin <tfasoyi2@my.centennialcollege.ca>
2026-04-09 13:40:27 -0400

    Add project files.

---
+++ StringExtensionsLib/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace StringExtensionsLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string str)
+        {
+            return !string.IsNullOrEmpty(str) && char.IsUpper(str[0]);
+        }
+    }
+}
